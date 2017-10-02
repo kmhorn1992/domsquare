5560f2594ad2b5288f941002b5c15e8004c7f93d
Adrian <kmhorn1992@icloud.com>
2017-10-02 13:14:12 -0500

    2nd commit

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var mybtn = document.createElement('button');
-    mybtn.innerText = ('Add Square');
-    document.body.appendChild('mybtn');
-
-
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-       
-        return color;
-
-    
+    console.log('DOMContentLoaded');
     
 
-        function setRandomColor() {
-            $("#colorpad").css("background-color", getRandomColor());
+    var mybtn = document.getElementById('mybtn');
+    mybtn.className = 'squarebutton';
+    mybtn.innerText = 'Add Square';
+    document.body.appendChild = (mybtn);
+
+});
 
 
-            mybtn.addEventListener('click', function () {
-                var squareDiv = document.createElement('div');
-                squareDiv.className = ('box');
-                document.body.appendChild(squareDiv);
-                var squareId = document.getElementsByTagName('div');
-                squareDiv.id = squareId.length;
 
-                var squarenum = document.createElement('h3');
-                squarenum.innerText = (squareId.length);
-                squareDiv.appendChild(squarenum);
-            });
-        });
-    });
-        });
+
+  
 
 
         
 
-
-
-
-
-
+         
+        
+        
+       
